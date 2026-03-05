a4032d4342891cce7167462fc2a6e5354b74ce0a
j-nyamu <j.mungiria@alustudent.com>
2026-03-05 05:36:11 -0800

    a

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from sys import argv
 
-total = 0
-for arg in argv[1:]:
-    total += int(arg)
-print(total)
+if __name__ == "__main__":
+    total = 0
+    for arg in argv[1:]:
+        total += int(arg)
+    print(total)
