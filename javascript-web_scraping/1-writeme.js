e9cd486ba8fd2d766e3fde31d2429262361453a4
j-nyamu <j.mungiiria@alustudent.com>
2026-06-25 15:12:37 +0000

    f

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -12,18 +12,4 @@ fs.writeFile(filePath, content, 'utf-8', function (err) {
   if (err) {
     console.log(err);
   }
-});#!/usr/bin/node
-// Write a string to a file using the fs module
-const fs = require('fs');
-
-// First argument is the file path, second is the string to write
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-// Write content to file in utf-8 encoding
-fs.writeFile(filePath, content, 'utf-8', function (err) {
-  // If error occurred, print the error object
-  if (err) {
-    console.log(err);
-  }
 });
